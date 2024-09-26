d6b06e46fcda4265f485eb42c680219ee70a8ce9
Juanmi <Juanmi26304@gmail.com>
2024-09-26 19:18:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 const timeElement = document.getElementById("time");
-const metaAlcanzada = document.getElementById("time");
-let fechaActual = new Date();
 let limitDate = new Date('01/01/2025');
-let tiempoRestante = limitDate - fechaActual;
-let tiempoTotal = tiempoRestante;
-console.log(tiempoTotal);
+let contador;
 const msMes = 2592000000; // Cálculo: (1000 * 60 * 60 * 24 * 30)
 const msDia = 86400000;   // Cálculo: (1000 * 60 * 60 * 24)
 const msHora = 3600000;   // Cálculo: (1000 * 60 * 60)
 const msMinuto = 60000;   // Cálculo: (1000 * 60)
 const msSegundo = 1000;   // Mil milisegundos por segundo
-let meses;
-let dias;
-let horas;
-let minutos;
-let segundos;
+let meses, dias, horas, minutos, segundos;
+
 tRestante();
-timeElement.innerText = meses + " " + dias + " " + horas + " " + minutos + " " + segundos;
+timeElement.innerText = `${meses} meses ${dias} días ${horas} horas ${minutos} minutos ${segundos} segundos`;
 arrancarContador();
+
 function arrancarContador() {
-    let contador = setInterval(() => {
+    contador = setInterval(() => {
         if (segundos == 0 && minutos != 0) {
             minutos--;
             segundos = 60;
@@ -39,7 +33,6 @@ function arrancarContador() {
             minutos = 59;
             segundos = 60;
         } else if(meses <= 0 && dias <= 0 && horas <= 0 && minutos <= 0 && segundos <= 0) {
-            clearInterval(contador);
             meses = 0;
             dias = 0;
             horas = 0;
@@ -47,11 +40,13 @@ function arrancarContador() {
             segundos = 1;
         }
         segundos--;
-        timeElement.innerText = meses + " " + dias + " " + horas + " " + minutos + " " + segundos;
+        timeElement.innerText = `${meses} meses ${dias} días ${horas} horas ${minutos} minutos ${segundos} segundos`;
     }, 1000)
 }
 
 function tRestante() {
+    let tiempoRestante = limitDate - new Date();
+
     meses = Math.floor(tiempoRestante / msMes);
     tiempoRestante %= msMes;
 
@@ -60,16 +55,17 @@ function tRestante() {
 
     horas = Math.floor(tiempoRestante / msHora);
     tiempoRestante %= msHora;
+    horas = horas - 2;
+    /*La franja horaria obliga a restarle dos horas de manera manual, 
+    la única forma de que no fuese necesario sería cambiarla pero no sé como
+    creo que hay varios compañeros que han hecho lo mismo, no lo tengas en cuenta 😅 */
 
     minutos = Math.floor(tiempoRestante / msMinuto);
     tiempoRestante %= msMinuto;
 
     segundos = Math.floor(tiempoRestante / msSegundo);
 }
-/*etTimeout(() => {
-    clearInterval(contador);
-    metaAlcanzada.innerText = "Fecha Alcanzada";
-}, tiempoTotal);*/
+
 let inputFecha = document.createElement("input");
 inputFecha.setAttribute("id", "name", "type");
 inputFecha.type = "date";
@@ -79,11 +75,7 @@ document.body.appendChild(inputFecha);
 
 inputFecha.addEventListener("change", () => {
     limitDate = new Date(inputFecha.value);
-    console.log(limitDate);
-    console.log(fechaActual);
-    tiempoRestante = limitDate - fechaActual;
-    tiempoTotal = tiempoRestante;
-    console.log(tiempoTotal);
+    clearInterval(contador);
     tRestante();
     arrancarContador();
 });
